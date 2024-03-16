d9fc39fedcbc247136153dec0a7dc2669a9cd890
MilaMi94 <mila.bogdanovic94@gmail.com>
2024-03-16 23:05:36 +0100

    remove useMaterial3

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
+        //useMaterial3: true,
         colorScheme: kDarkColorScheme,
         cardTheme: const CardTheme().copyWith(
           color: kDarkColorScheme.secondaryContainer,
@@ -32,7 +32,7 @@ void main() {
         ),
       ),
       theme: ThemeData().copyWith(
-        useMaterial3: true,
+        // useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
